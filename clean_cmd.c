9468c075702323b4e9b66ff504586a00118d1f72
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-05-23 18:59:21 +0100

    [update] add function clean_cmd that cleans the commande

--- clean_cmd.c
+++ clean_cmd.c
@@ -9,6 +9,35 @@
 
 void clean_shell(void)
 {
+	char **buffer;
+	int i = 0, cmd_type = 0, line;
+	size_t size = 0;
 
+	if (!(isatty(STDIN_FILENO)))
+	{
+		line = getlin(&ln, &size, stdin);
+		while (line != -1)
+		{
+			rm_newline(ln);
+			rm_comment(ln);
+			cmd = strtok(ln, ";");
+			while (cmd[i] != 0)
+			{
+				buffer = strtok(cmd[i], " ");
+				if (buffer[0] == 0)
+				{
+					free(buffer);
+					break;
+				}
+				cmd_type = parsing_cmd(buffer[0]);
+				ready_cmd(buffer, cmd_type);
+				free(buffer);
+				i++;
+			}
+			free(cmd);
+		}
+		free(ln);
+		exit(st);
+	}
 }
 
