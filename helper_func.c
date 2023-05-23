d8c77fca451cc7c1ff933c2b9c8fe19bd0a4d44b
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-05-23 15:03:17 +0100

    add helper function to help us in this project

---
+++ helper_func.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * display - display prompt message.
+ *
+ * Return: 0.
+ */
+void display()
+{
+	printf("#cisfun$ ");
+}
+
+/**
+ * print_err - print error.
+ *
+ * Return: 0.
+ */
+void print_err()
+{
+	printf("./shell: No such file or directory");
+}
+
+/**
+ * rm_newline - remove a new line from a commande.
+ * @cmd: the commande..
+ *
+ * Return: 0.
+ */
+
+void rm_newline(char *cmd)
+{
+	int i = 0;
+
+	while (cmd[i] != 0)
+	{
+		if (cmd[i] == '\n')
+			break;
+		i++;
+	}
+	cmd[i] = 0;
+}
+
+/**
+ * rm_comment - remove comments from a command.
+ * @cmd: the commande.
+ *
+ * Return: 0.
+ */
+
+void rm_comment(char *cmd)
+{
+	int i = 0;
+
+	if (cmd[i] == '#')
+		cmd[i] = 0;
+	while (cmd[i] != 0)
+	{
+		if (cmd[i] == '#' && cmd[i - 1] == ' ')
+			break;
+		i++;
+	}
+	cmd[i] = 0;
+}
